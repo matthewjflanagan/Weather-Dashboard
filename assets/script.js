30c08db1776c002af4e81ff6947e9965c33f810d
Matthew Flanagan <mattflanagan40@gmail.com>
2022-06-29 15:19:33 -0400

    added info

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     var cityName = $('.city-title');
     var pic = $('#current-pic');
     var tempEl = $('#temperature');
+    var tempElR = $('#temperatureRange');
     var humidityEl = $('#humidity');
     var windEl = $('#wind-speed');
     var uvIndex = $('#UV-index');
@@ -25,11 +26,11 @@ $(document).ready(function () {
             .then(function (data) {
                  console.log(data)
                 displayCurrentWeather(data)
-                getUVIndex(data);
+                fiveDayForecast(data);
             })
     };
 
-    function getUVIndex (data) {
+    function fiveDayForecast (data) {
         var lat = data.coord.lat
         var lon = data.coord.lon
         var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly" + "&appid=" + APIKey;
@@ -47,33 +48,35 @@ $(document).ready(function () {
         })
         .then(function(data) {
             uvIndex.text("UV Index: " + data.current.uvi);
-            $('#day1temp').text("Temperature: " + k2f(data.daily[1].temp.day));
-            $('#day1wind').text("Wind Speed: " + data.daily[1].wind_speed);
-            $('#day1humidity').text("Humidity: " + data.daily[1].humidity);
+            $('#day1temp').text("Average Temp: " + k2f(data.daily[1].temp.day) + "° F" + " & Feels like: " + k2f(data.daily[1].feels_like.day) + "° F");
+            $('#day1wind').text("Wind Speed: " + data.daily[1].wind_speed + " mph");
+            $('#day1humidity').text("Humidity: " + data.daily[1].humidity + "%");
 
-            $('#day2temp').text("Temperature: " + k2f(data.daily[2].temp.day));
-            $('#day2wind').text("Wind Speed: " + data.daily[2].wind_speed);
-            $('#day2humidity').text("Humidity: " + data.daily[2].humidity);
+            $('#day2temp').text("Average Temp: " + k2f(data.daily[2].temp.day) + "° F" + " & Feels like: " + k2f(data.daily[2].feels_like.day) + "° F");
+            $('#day2wind').text("Wind Speed: " + data.daily[2].wind_speed + " mph");
+            $('#day2humidity').text("Humidity: " + data.daily[2].humidity + "%");
 
-            $('#day3temp').text("Temperature: " + k2f(data.daily[3].temp.day));
-            $('#day3wind').text("Wind Speed: " + data.daily[3].wind_speed);
-            $('#day3humidity').text("Humidity: " + data.daily[3].humidity);
+            $('#day3temp').text("Average Temp: " + k2f(data.daily[3].temp.day) + "° F" + " & Feels like: " + k2f(data.daily[3].feels_like.day) + "° F");
+            $('#day3wind').text("Wind Speed: " + data.daily[3].wind_speed + " mph");
+            $('#day3humidity').text("Humidity: " + data.daily[3].humidity + "%");
 
-            $('#day4temp').text("Temperature: " + k2f(data.daily[4].temp.day));
-            $('#day4wind').text("Wind Speed: " + data.daily[4].wind_speed);
-            $('#day4humidity').text("Humidity: " + data.daily[4].humidity);
+            $('#day4temp').text("Average Temp: " + k2f(data.daily[4].temp.day) + "° F" + " & Feels like: " + k2f(data.daily[4].feels_like.day) + "° F");
+            $('#day4wind').text("Wind Speed: " + data.daily[4].wind_speed + " mph");
+            $('#day4humidity').text("Humidity: " + data.daily[4].humidity + "%");
 
-            $('#day5temp').text("Temperature: " + k2f(data.daily[5].temp.day));
-            $('#day5wind').text("Wind Speed: " + data.daily[5].wind_speed);
-            $('#day5humidity').text("Humidity: " + data.daily[5].humidity);
+            $('#day5temp').text("Average Temp: " + k2f(data.daily[5].temp.day) + "° F" + " & Feels like: " + k2f(data.daily[5].feels_like.day) + "° F");
+            $('#day5wind').text("Wind Speed: " + data.daily[5].wind_speed + " mph");
+            $('#day5humidity').text("Humidity: " + data.daily[5].humidity + "%");
         })
     }
 
     function displayCurrentWeather(data) {
+        pic.text(data.weather[0].main);
+        tempElR.text("High: " + k2f(data.main.temp_max) + "° F" + " & Low: " + k2f(data.main.temp_min) + "° F");
         cityName.text("City Name: " + data.name);
-        tempEl.text("Temperature: " + k2f(data.main.temp));
-        humidityEl.text("Humidity: " + data.main.humidity);
-        windEl.text("Wind Speed: " + data.wind.speed);
+        tempEl.text("Current Temp: " + k2f(data.main.temp) + "° F & Feels like: " + k2f(data.main.feels_like) + "° F");
+        humidityEl.text("Humidity: " + data.main.humidity + "%");
+        windEl.text("Wind Speed: " + data.wind.speed + " mph");
     }
 
     searchButton.addEventListener("click", function () {
